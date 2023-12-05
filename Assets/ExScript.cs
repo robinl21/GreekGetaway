78d66c2a0c33aa9db8c16242a68d71c7ea71ae61
qcantu <98923114+qcantu@users.noreply.github.com>
2023-12-05 03:08:55 -0500

    Added in basic ex pathing and capture pausing

---
+++ Assets/ExScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExScript : MonoBehaviour
+{
+    [SerializeField] Transform[] Points;
+
+    [SerializeField] public static float moveSpeed = 2.5f;
+
+    private int pointsIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = Points[pointsIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(pointsIndex <= Points.Length - 1)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, Points[pointsIndex].transform.position, moveSpeed * Time.deltaTime);
+
+            if(transform.position == Points[pointsIndex].transform.position)
+            {
+                pointsIndex += 1;
+            }
+
+            if(pointsIndex == Points.Length)
+            {
+                pointsIndex = 0;
+            }
+        }
+    }
+}

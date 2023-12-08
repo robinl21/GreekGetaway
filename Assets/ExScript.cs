8b5c800ec1ae64b97a06ca466daaee2d939faae4
qcantu <98923114+qcantu@users.noreply.github.com>
2023-12-08 02:40:23 -0500

    Made exes have more complex routes and look in direction of movement

--- Assets/ExScript.cs
+++ Assets/ExScript.cs
@@ -6,9 +6,16 @@ public class ExScript : MonoBehaviour
 {
     [SerializeField] Transform[] Points;
 
-    [SerializeField] public static float moveSpeed = 2.5f;
+    [SerializeField] public static float moveSpeed = 3.55f;
 
     private int pointsIndex = 0;
+    private float prevX = 0;
+    private float prevY = 0;
+
+    public Sprite rightSprite;
+    public Sprite leftSprite;
+    public Sprite upSprite;
+    public Sprite downSprite;
 
     // Start is called before the first frame update
     void Start()
@@ -22,8 +29,28 @@ public class ExScript : MonoBehaviour
         if(pointsIndex <= Points.Length - 1)
         {
             transform.position = Vector2.MoveTowards(transform.position, Points[pointsIndex].transform.position, moveSpeed * Time.deltaTime);
+            //change sprite direction
+            if (prevY < transform.position[1])
+            {
+                transform.Find("NPC").gameObject.GetComponent<SpriteRenderer>().sprite = upSprite;
+            }
+            else if (prevY > transform.position[1])
+            {
+                transform.Find("NPC").gameObject.GetComponent<SpriteRenderer>().sprite = downSprite;
+            }
+            else if (prevX < transform.position[0])
+            {
+                transform.Find("NPC").gameObject.GetComponent<SpriteRenderer>().sprite = rightSprite;
+            }
+            else if (prevX > transform.position[0])
+            {
+                transform.Find("NPC").gameObject.GetComponent<SpriteRenderer>().sprite = leftSprite;
+            }
 
-            if(transform.position == Points[pointsIndex].transform.position)
+            prevX = transform.position[0];
+            prevY = transform.position[1];
+
+            if (transform.position == Points[pointsIndex].transform.position)
             {
                 pointsIndex += 1;
             }
--- Assets/ExScript2.cs
+++ Assets/ExScript2.cs
@@ -6,9 +6,16 @@ public class ExScript2 : MonoBehaviour
 {
     [SerializeField] Transform[] Points;
 
-    [SerializeField] public static float moveSpeed = 2.5f;
+    [SerializeField] public static float moveSpeed = 3.5f;
 
     private int pointsIndex = 0;
+    private float prevX = 0;
+    private float prevY = 0;
+
+    public Sprite rightSprite;
+    public Sprite leftSprite;
+    public Sprite upSprite;
+    public Sprite downSprite;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +29,26 @@ public class ExScript2 : MonoBehaviour
         if (pointsIndex <= Points.Length - 1)
         {
             transform.position = Vector2.MoveTowards(transform.position, Points[pointsIndex].transform.position, moveSpeed * Time.deltaTime);
+            //change sprite direction
+            if (prevY < transform.position[1])
+            {
+                transform.Find("NPC").gameObject.GetComponent<SpriteRenderer>().sprite = upSprite;
+            }
+            else if (prevY > transform.position[1])
+            {
+                transform.Find("NPC").gameObject.GetComponent<SpriteRenderer>().sprite = downSprite;
+            }
+            else if (prevX < transform.position[0])
+            {
+                transform.Find("NPC").gameObject.GetComponent<SpriteRenderer>().sprite = rightSprite;
+            }
+            else if (prevX > transform.position[0])
+            {
+                transform.Find("NPC").gameObject.GetComponent<SpriteRenderer>().sprite = leftSprite;
+            }
+
+            prevX = transform.position[0];
+            prevY = transform.position[1];
 
             if (transform.position == Points[pointsIndex].transform.position)
             {

21620ac8a2900cc89a666d73dafa51a31465d8d9
Kudana Gwatidzo <68567108+harvardkudana@users.noreply.github.com>
2023-12-11 00:38:44 -0500

    Integrated music!
    Finally
    - Completed mixing for club and drunk music
    - Menu music integrated in introduction scene
    - Music transition when drunk
    - Music transition when time is running out.

--- Assets/Scripts/TimerScript.cs
+++ Assets/Scripts/TimerScript.cs
@@ -11,11 +11,18 @@ public class TimerScript : MonoBehaviour
     private float originalTime;
     public TextMeshProUGUI timerVisual;
 
+    private AudioSource audioSource;
+    public AudioClip[] music;
+    private bool[] hasMusicBeenPlayed = {false, false, false};
     // Start is called before the first frame update
     void Start()
     {
         timerVisual = GameObject.Find("Timer").GetComponent<TextMeshProUGUI>();
         originalTime = totalTime;
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = music[0];
+        audioSource.Play();
+        hasMusicBeenPlayed[0] = true;
     }
 
     // Update is called once per frame
@@ -31,7 +38,24 @@ public class TimerScript : MonoBehaviour
         }
         float minutes = Mathf.FloorToInt(totalTime / 60);  
         float seconds = Mathf.FloorToInt(totalTime % 60);
-        if (totalTime <= originalTime - 45) timerVisual.text = string.Format("DRUNK\n{0:00}:{1:00}", minutes, seconds);
+        if (totalTime <= originalTime - 45) {
+            timerVisual.text = string.Format("DRUNK\n{0:00}:{1:00}", minutes, seconds);
+            // activate the drunk timer and the music
+            if (!hasMusicBeenPlayed[1])
+            {
+                audioSource.Stop();
+                audioSource.clip = music[1];
+                audioSource.Play();
+                hasMusicBeenPlayed[1] = true;
+            }
+            if (totalTime < 13 && !hasMusicBeenPlayed[2])
+            {
+                audioSource.Stop();
+                audioSource.clip = music[2];
+                audioSource.Play();
+                hasMusicBeenPlayed[2] = true;
+            }
+        }
         else timerVisual.text = string.Format("{0:00}:{1:00}", minutes, seconds);
         if (totalTime <= originalTime / 2) timerVisual.color = Color.red;
     }
